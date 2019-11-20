449ee409102b4644012e067e577fe4a1cae44918
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-20 15:37:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const { exec } = require('child_process');
+const express = require("express");
 
+exec('cat *.js bad_file | wc -l', (err, stdout, stderr) => {
+  if (err) {
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+
+const app = express();
+
+
+app.get("/", function(req, res) {
+  var r = req.query
+  
+});
+
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Listening on port " + listener.address().port);
+});
